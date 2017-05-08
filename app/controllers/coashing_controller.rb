840d3fb3e6c26903a85cab1c3e2f3e1708d062ab
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-05-08 13:07:54 +0200

    done

--- app/controllers/coashing_controller.rb
+++ app/controllers/coashing_controller.rb
@@ -1,5 +1,17 @@
 class CoashingController < ApplicationController
   def answer
+    @query = params[:query]
+    case @query
+    when 'what time is it ?'
+    @answer = Time.now.asctime
+  when @query.include?('?')
+    @answer = 'Silly question, get dressed and go to work!'
+    when "I am going to work right now!"
+    @answer = 'Cool !'
+    else
+    @answer = "I don't care, get dressed and go to work!"
+    end
+
   end
 
   def ask
